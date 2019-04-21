0a71a1a562d867986ae257faa163c8ff91182b97
Alexander Dawson <alexdee17@gmail.com>
2019-04-21 19:41:05 -0400

    cleaned up some code

--- main.js
+++ main.js
@@ -4,24 +4,30 @@
 function getDogImages(userNum) {
     if (userNum > 50) {
         return alert("Please choose a number equal or less than 50");
-} else {
-    fetch(`https://dog.ceo/api/breeds/image/random/${userNum}`)
-      .then(response => response.json())
-      .then(responseJson => displayResults(responseJson))
-      .catch(error => alert('Something went wrong. Try again later.'));
-  }
+    } else {
+        fetch(`https://dog.ceo/api/breeds/image/random/${userNum}`)
+            .then(response => response.json())
+            .then(displayResults)
+            .catch(error => alert('Something went wrong. Try again later.'));
+    }
 }
+
 function displayResults(responseJson) {
     const imageList = responseJson.message;
     console.log(imageList);
-    let imageUrls = "";
-  
-    for (let x = 0;x < imageList.length;x++) {
-      imageUrls += `<img src="${imageList[x]}" alt="Generated dog image # ${x+1}" class="loadedImages">`;
-    }
-  
+
+    const imageUrls = imageList
+        .map((imageUrl, index) => 
+            `<img src="${imageUrl}" alt="Generated dog image #${index+1}" class="loadedImages">`
+        )
+        .join('');
+
+    // const imageUrls = imageList.reduce((string, imageUrl, index) =>
+    //     string += `<img src="${imageUrl}" alt="Generated dog image #${index+1}" class="loadedImages">`
+    // , "");
+
     console.log(imageUrls);
-  
+
     $('.images').replaceWith(`<div class="images">${imageUrls}</div>`);
 }
 
@@ -35,9 +41,7 @@ function listenForUserNum() {
 }
 
 
-$(function() {
-  console.log("App loaded! Waiting for submit!");
-  listenForUserNum();
+$(function () {
+    console.log("App loaded! Waiting for submit!");
+    listenForUserNum();
 });
-
-
